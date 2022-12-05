968072217dd9833f94159ea357ff127505a82df9
KIRYL_CHOPIK <kichvic@gmail.com>
2022-12-05 13:19:42 +0300

    accept alert

---
+++ 2_3_4_auto_test.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try: 
+
+    link = "http://suninjuly.github.io/alert_accept.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    button_first = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button_first.click()
+
+    confirm = browser.switch_to.alert
+    confirm.accept()
+
+    x_element = browser.find_element(By.CSS_SELECTOR, '#input_value')
+    x = x_element.text
+    y = calc(x)
+
+    answer = browser.find_element(By.CSS_SELECTOR, '#answer')
+    answer.send_keys(y)
+
+    button_submit = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button_submit.click()
+
+    
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
